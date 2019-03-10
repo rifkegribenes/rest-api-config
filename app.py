ba9b17659434d313075a7232dd485052341d1fc8
rifkegribenes <rifkegribenes@gmail.com>
2019-03-10 13:55:26 -0700

    avatar resource

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogo
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload, Image
+from resources.image import ImageUpload, Image, AvatarUpload, Avatar
 from libs.image_helper import IMAGE_SET
 
 
@@ -22,9 +22,8 @@ app.config.from_object("default_config")  # load default configs from default_co
 app.config.from_envvar(
     "APPLICATION_SETTINGS"
 )  # override with config.py (APPLICATION_SETTINGS points to config.py)
-patch_request_class(app, 10 * 1024 * 1024) # 10MB file size limit
+patch_request_class(app, 10 * 1024 * 1024)  # restrict max upload image size to 10MB
 configure_uploads(app, IMAGE_SET)
-
 api = Api(app)
 
 
@@ -60,8 +59,10 @@ api.add_resource(Confirmation, "/user_confirm/<string:confirmation_id>")
 api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
 api.add_resource(ImageUpload, "/upload/image")
 api.add_resource(Image, "/image/<string:filename>")
+api.add_resource(AvatarUpload, "/upload/avatar")
+api.add_resource(Avatar, "/avatar/<int:user_id>")
 
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
